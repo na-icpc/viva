ea24998104d4156562f50b80ceeb82d71b6299c9
sfvanb <8527390+vanbgit@users.noreply.github.com>
2011-06-03 20:33:09 +0000

    Added bitwise operators, unary operators.

---
+++ Input Verifier/src/org/vanb/viva/expressions/NegativeNode.java
@@ -0,0 +1,42 @@
+package org.vanb.viva.expressions;
+
+import org.vanb.viva.parser.ParseException;
+import org.vanb.viva.utils.*;
+
+public class NegativeNode extends UnaryOperatorNode
+{
+    Class<?> type;
+    
+    @Override
+    public void instantiate( ExpressionNode arg ) throws ParseException
+    {
+        type = arg.getReturnType();
+        if( !Number.class.isAssignableFrom( type ) )
+        {
+            throw new ParseException( "Bad operand to " + operator + " operator: Expecting Integer, Long, Double or Float, got " + arg.getReturnType().getSimpleName() );  
+        }
+        argument = arg;
+    }
+
+    @Override
+    public Object evaluate( VIVAContext context ) throws VIVAException
+    {
+        Object arg = argument.evaluate( context );
+        Object result;
+        
+        if( type==Long.class ) result = Long.valueOf( -(Long)arg );
+        else if( type==Integer.class ) result = Integer.valueOf( -(Integer)arg );
+        else if( type==Double.class ) result = Double.valueOf( -(Double)arg );
+        else result = Float.valueOf( -(Float)arg );
+        
+        return  result;
+    }
+
+    @Override
+    public Class<?> getReturnType()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ Input Verifier/src/org/vanb/viva/expressions/NotNode.java
@@ -0,0 +1,32 @@
+package org.vanb.viva.expressions;
+
+import org.vanb.viva.parser.ParseException;
+import org.vanb.viva.utils.*;
+
+public class NotNode extends UnaryOperatorNode
+{
+
+    @Override
+    public void instantiate( ExpressionNode arg ) throws ParseException
+    {
+        if( arg.getReturnType()!=Boolean.class )
+        {
+            throw new ParseException( "Bad operand to " + operator + " operator: Expecting Boolean, got " + arg.getReturnType().getSimpleName() );  
+        }
+        argument = arg;
+    }
+
+    @Override
+    public Object evaluate( VIVAContext context ) throws VIVAException
+    {
+        return ! (Boolean)argument.evaluate( context );
+    }
+
+    @Override
+    public Class<?> getReturnType()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
